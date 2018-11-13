1a74bd8ed2b5b153b4bcafeb771a42a94c3462ed
RehaS <beratson@gmail.com>
2018-11-13 18:17:35 +0100

    plan update, cors addition

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const app = require('express')();
 var bodyParser = require('body-parser');
+var cors = require('cors')
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://amsnow:amsnow1@ds261253.mlab.com:61253/mern-assesment-amsn', {useNewUrlParser: true});
 
+app.use(cors());
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
